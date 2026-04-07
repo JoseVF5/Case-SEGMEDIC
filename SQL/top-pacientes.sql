921ef9db13492d9486700e4c855207c729f8d8e1
JoseVF5 <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>
2026-04-07 20:03:15 -0300

    test: testando o order by para refatoramento do código

--- SQL/top-pacientes.sql
+++ SQL/top-pacientes.sql
@@ -1,7 +1,7 @@
 select
 	id_paciente,
 	SUM(case when especialidade = 'Psiquiatria' then valor else 0 END) as psiq_faturamento,
-	SUM(case when especialidade = 'Clínica Geral'then valor else 0 END) as clinica_faturamento,
+	SUM(case when especialidade = 'Clínica Geral'then valor else 0 END) as clin_geral_faturamento,
 	SUM(case when especialidade = 'Endocrinologia'then valor else 0 END) as endo_faturamento,
 	COUNT(id_agendamento) as quant_agendamentos
 from
@@ -11,4 +11,5 @@ where
 group by 
 	id_paciente
 order by
-  	 psiq_faturamento DESC limit 10;
+  	 clin_geral_faturamento desc
+limit 10;
