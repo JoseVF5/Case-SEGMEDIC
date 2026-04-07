c0ed74e70a871713cfa6c221a22926b5e7b49f14
JoseVF5 <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>
2026-04-07 20:12:38 -0300

    fix: where in para filtar

--- SQL/top-pacientes.sql
+++ SQL/top-pacientes.sql
@@ -1,15 +1,14 @@
-select
-	id_paciente,
-	SUM(case when especialidade = 'Psiquiatria' then valor else 0 END) as psiq_faturamento,
-	SUM(case when especialidade = 'Clínica Geral'then valor else 0 END) as clin_geral_faturamento,
-	SUM(case when especialidade = 'Endocrinologia'then valor else 0 END) as endo_faturamento,
-	COUNT(id_agendamento) as quant_agendamentos
+select 
+    id_paciente,
+    SUM(valor) as faturamento,
+    COUNT(id_agendamento) as quant_atendimentos
 from
-	atendimentos
-where 
-	id_paciente is not null
-group by 
-	id_paciente
+    atendimentos
+where
+    especialidade in ('Psiquiatria', 'Clínica Geral', 'Endocrinologia') 
+group by
+    id_paciente
 order by
-  	 clin_geral_faturamento desc
+    faturamento desc,
+    quant_atendimentos desc
 limit 10;
